181372b27f1a62dfa0daf61de19a0e0628341856
Andrew Au <andrew.au@cbc.ca>
2017-01-21 17:49:11 -0500

    fixed ss.js and created screencap script

--- js/ss.js
+++ js/ss.js
@@ -1,11 +1,12 @@
 var page = require('webpage').create();
+var system = require('system');
 var args = system.args;
 
-var url = args[0];
-var out = args[1];
+var url = args[1];
+var out = args[2];
 
-if (!!args[2] && !!args[3]) {
-	page.viewportSize = { width: args[2], height: args[3] };
+if (!!args[3] && !!args[4]) {
+	page.viewportSize = { width: args[3], height: args[4] };
 }
 page.open(url, function() {
 	page.render(out);
