ffc0a504465b3e12041d290fd83239111d0a872d
Andrew Au <andrew.au@cbc.ca>
2017-01-22 11:51:42 -0500

    fixed install errors on linux

--- install.sh
+++ install.sh
@@ -6,17 +6,18 @@ if [[ $OS == "Darwin" ]]; then
 	if [[ -z `type -p brew` ]]; then
 		echo "Please install brew"
 	else
-		if [[ -z `type -p brew` ]]; then
+		if [[ -z `type -p phantomjs` ]]; then
 			echo "Installing PhantomJS"
 			brew install phantomjs
+		else
+			echo "PhantomJS already installed"
 		fi
 	fi
 elif [[ $OS == "Linux" ]]; then
-	if [[ -z `type -p brew` ]]; then
-		echo "Installing PhantomJS"
-		sudo apt-get install -y phantomjs
-	fi
+	echo "Installing PhantomJS"
+	sudo apt-get install -y phantomjs
 fi
+
 cur_dir=`pwd`
 sudo ln -s $cur_dir'/screencap' '/usr/local/bin/screencap'
 echo "Install complete"
