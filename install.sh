5832ff0b24c99554225983cdc9f3b256fd3e4332
Andrew Au <andrew.au@cbc.ca>
2017-01-21 19:03:33 -0500

    added error message to screencap and fixed if statements in the install script

--- install.sh
+++ install.sh
@@ -3,19 +3,20 @@
 OS=`uname -s`
 
 if [[ $OS == "Darwin" ]]; then
-	if [[ ! [ brew_loc=`type -p brew` ] -o [ -z $brew_loc] ]]; then
+	if [[ -z `type -p brew` ]]; then
 		echo "Please install brew"
 	else
-		if [[ ! [ phsntmjs_loc=`type -p brew` ] -o [ -z $phntmjs_loc] ]]; then
+		if [[ -z `type -p brew` ]]; then
 			echo "Installing PhantomJS"
 			brew install phantomjs
 		fi
 	fi
 elif [[ $OS == "Linux" ]]; then
-	if [[ ! [ phsntmjs_loc=`type -p brew` ] -o [ -z $phntmjs_loc] ]]; then
+	if [[ -z `type -p brew` ]]; then
 		echo "Installing PhantomJS"
 		sudo apt-get install -y phantomjs
 	fi
 fi
-
+cur_dir=`pwd`
+sudo ln -s $cur_dir'/screencap' '/usr/local/bin/screencap'
 echo "Install complete"
